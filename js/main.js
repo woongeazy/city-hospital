4d278926e8d6c803cfcbaf4fe585e7d6519e4716
hanchaewoong <hco2995@naver.com>
2023-11-18 23:13:30 +0900

    메뉴 드롭바 완성

--- js/main.js
+++ js/main.js
@@ -1,43 +1,59 @@
-$(function(){
-    let currentIndex = 0;
-    $('.slider_wrap').append($('.slide1').first().clone(true));
+$(function () {
+  let currentIndex = 0;
+  $(".slider_wrap").append($(".slide1").first().clone(true));
 
-    setInterval(function(){
-        currentIndex++;
-        $('.slider_wrap').animate({marginLeft: -currentIndex * 100 + "%"}, 600);
+  setInterval(function () {
+    currentIndex++;
+    $(".slider_wrap").animate({ marginLeft: -currentIndex * 100 + "%" }, 600);
 
-        if(currentIndex == 3){
-            setTimeout(function(){
-                $(".slider_wrap").animate({marginLeft: 0}, 0);
-                currentIndex = 0;
-            }, 700);
-        }
-    }, 3000);
+    if (currentIndex == 3) {
+      setTimeout(function () {
+        $(".slider_wrap").animate({ marginLeft: 0 }, 0);
+        currentIndex = 0;
+      }, 700);
+    }
+  }, 3000);
 
-    let tabBtn = $('.tab_title > li');
-    let tabCont = $('.tab_contents > div')
+  let tabBtn = $(".tab_title > li");
+  let tabCont = $(".tab_contents > div");
 
-    tabCont.hide().eq(0).show();
-    
-    tabBtn.on('click', function(){
-        const index = $(this).index()
-        tabCont.eq(index).show().siblings().hide()
-    });
+  tabCont.hide().eq(0).show();
 
-    lnbBtn.on('mouseover', function(){
-        $('nav, .lnb').addClass("on");
-    });
+  tabBtn.on("click", function () {
+    const index = $(this).index();
+    tabCont.eq(index).show().siblings().hide();
+  });
 
-    lnbBtn.on('mouseout', function(){
-        $('nav, .lnb').removeClass("on");
-    });
-    
-    // toggleClass 클래스를 추가하거나 제거
-    $('.gnb > li:nth-child(1)').on('click', function(){
-        $('nav, .lnb').toggleClass("on");
-    });
+  lnbBtn.on("mouseover", function () {
+    $("nav, .lnb").addClass("on");
+  });
+
+  lnbBtn.on("mouseout", function () {
+    $("nav, .lnb").removeClass("on");
+  });
+
+  // toggleClass 클래스를 추가하거나 제거
+  $(".gnb > li:nth-child(1)").on("click", function () {
+    $("nav, .lnb").toggleClass("on");
+  });
+
+  $(".gnb li").on("mouseover", function () {
+    $(this).css({ background: "var(--color-1)", color: "var(--font-color" });
+  });
+
+  $(".gnb li").on("mouseout", function () {
+    $(this).css({background: "none", color: "#000"});
+  });
+
+  $(".gnb > li:nth-last-child(1)").on("click", function () {
+    selectWind.toggleClass("se");
+  });
 });
 
 let menuTogg = 0;
 
-let lnbBtn = $('.gnb > li:nth-child(2), .gnb > li:nth-child(3), .gnb > li:nth-child(4), .gnb > li:nth-child(5), .gnb > li:nth-child(6)')
+let selectWind = $("nav, nav > form");
+
+let lnbBtn = $(
+  ".gnb > li:nth-child(2), .gnb > li:nth-child(3), .gnb > li:nth-child(4), .gnb > li:nth-child(5), .gnb > li:nth-child(6)"
+);
